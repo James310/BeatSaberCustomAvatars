47122cc60aac7d8e5cf3293c0905279820a826d2
Nicolas Gnyra <nicolas@gnyra.com>
2020-10-13 08:57:14 -0400

    Temporarily disable UI

--- Source/CustomAvatar/Zenject/UIInstaller.cs
+++ Source/CustomAvatar/Zenject/UIInstaller.cs
@@ -25,18 +25,18 @@ namespace CustomAvatar.Zenject
     {
         public override void InstallBindings()
         {
-            var avatarListViewController = BeatSaberUI.CreateViewController<AvatarListViewController>();
-            var mirrorViewController = BeatSaberUI.CreateViewController<MirrorViewController>();
-            var settingsViewController = BeatSaberUI.CreateViewController<SettingsViewController>();
+            //var avatarListViewController = BeatSaberUI.CreateViewController<AvatarListViewController>();
+            //var mirrorViewController = BeatSaberUI.CreateViewController<MirrorViewController>();
+            //var settingsViewController = BeatSaberUI.CreateViewController<SettingsViewController>();
 
-            Container.Bind<AvatarListViewController>().FromInstance(avatarListViewController);
-            Container.Bind<MirrorViewController>().FromInstance(mirrorViewController);
-            Container.Bind<SettingsViewController>().FromInstance(settingsViewController);
-            Container.BindInterfacesAndSelfTo<AvatarMenuFlowCoordinator>().FromNewComponentOnNewGameObject(nameof(AvatarMenuFlowCoordinator));
+            //Container.Bind<AvatarListViewController>().FromInstance(avatarListViewController);
+            //Container.Bind<MirrorViewController>().FromInstance(mirrorViewController);
+            //Container.Bind<SettingsViewController>().FromInstance(settingsViewController);
+            //Container.BindInterfacesAndSelfTo<AvatarMenuFlowCoordinator>().FromNewComponentOnNewGameObject(nameof(AvatarMenuFlowCoordinator));
 
-            Container.QueueForInject(avatarListViewController);
-            Container.QueueForInject(mirrorViewController);
-            Container.QueueForInject(settingsViewController);
+            //Container.QueueForInject(avatarListViewController);
+            //Container.QueueForInject(mirrorViewController);
+            //Container.QueueForInject(settingsViewController);
         }
     }
 }
